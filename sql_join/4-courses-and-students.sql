463e80dfde50e6b04187c49dbddab0b87b400f79
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-03 19:11:07 +0200

    fix: add missing semicolon at the end of SQL query

--- sql_join/4-courses-and-students.sql
+++ sql_join/4-courses-and-students.sql
@@ -4,4 +4,4 @@ LEFT JOIN enrollments
 ON course_id = courses.id
 LEFT JOIN students
 ON student_id = students.id
-ORDER BY course_title ASC, student_name ASC
+ORDER BY course_title ASC, student_name ASC;
