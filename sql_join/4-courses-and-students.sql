5317d398760b3b68ba41a550c366026a71ae7ad5
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-03 18:58:10 +0200

    add SQL query to list courses with their corresponding student names

---
+++ sql_join/4-courses-and-students.sql
@@ -0,0 +1,7 @@
+SELECT courses.title AS course_title, students.name AS student_name
+FROM courses
+LEFT JOIN enrollments
+ON course_id = courses.id
+LEFT JOIN students
+ON student_id = students.id
+ORDER BY course_title ASC, student_name ASC
